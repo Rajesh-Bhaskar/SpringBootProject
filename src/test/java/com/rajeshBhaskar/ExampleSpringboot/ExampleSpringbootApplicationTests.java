2af6d02bfbf8f02c6bd68efa4e690b9e6c8f5069
Rajesh-Bhaskar <50402327+Rajesh-Bhaskar@users.noreply.github.com>
2024-03-07 12:32:26 +0530

    initial push

---
+++ src/test/java/com/rajeshBhaskar/ExampleSpringboot/ExampleSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajeshBhaskar.ExampleSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
