621ccb8f2036bab2e3469c7434f36514cc02707c
Rajesh-Bhaskar <50402327+Rajesh-Bhaskar@users.noreply.github.com>
2024-03-11 10:31:32 +0530

    Postgre DB implementation

---
+++ src/main/java/com/rajeshBhaskar/ExampleSpringboot/repository/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.rajeshBhaskar.ExampleSpringboot.repository;
+
+
+import com.rajeshBhaskar.ExampleSpringboot.model.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+@Repository
+public interface EmployeeRepository
+        extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
